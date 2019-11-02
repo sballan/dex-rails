43f98ddc69bfff421774e31ca848a58971bbf9bc
Samuel Ballan <sgb4622@gmail.com>
2019-11-01 23:17:53 -0400

    links

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,8 +3,17 @@ class Page < ApplicationRecord
 
   serialize :links, JSON
 
+  validates :links, presence: true
+
   def document
     require 'nokogiri'
     Nokogiri::HTML.parse(body)
   end
+
+  def create_urls_for_links
+    links.map do |link|
+      Url.create value: link
+    end
+  end
+
 end

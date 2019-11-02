a0b4c125f242ca8653eb21779088e1c3d49f8838
Samuel Ballan <sgb4622@gmail.com>
2019-11-02 11:42:54 -0400

    follow url job

--- app/jobs/follow_url_job.rb
+++ app/jobs/follow_url_job.rb
@@ -1,34 +1,38 @@
 class FollowUrlJob < ApplicationJob
   queue_as :default
 
-  def perform(url_id: url_id, counter: 1)
+  def perform(page_url:, counter: 1)
     return if counter < 1
-
     counter -= 1
-    page_url = url = Url.find url_id
 
-    page = Page.create(
-      url: page_url,
-      body: url.mechanize_page.body,
-      title: url.mechanize_page.title,
-      links: url.mechanize_page.links.map(&:resolved_uri )
-    )
+    if page_url.is_a? Integer
+      page_url = Url.find page_url
+    end
+
+
+    unless (page = Page.find_by(url: page_url))
+      page = create_page_for_url page_url
+    end
 
     link_urls = page.create_urls_for_links
 
-    link_urls.map do | url |
-      create_page_for_url(url)
-      perform(url_id: url.id, counter: counter)
+    link_urls.map do | link_url |
+      self.class.perform_later(page_url: link_url.id, counter: counter)
     end
 
+    link_urls.map do | url|
+      create_page_for_url(url)
+    end
   end
 
   def create_page_for_url(url)
+    mechanize_page = url.mechanize_page
+
     Page.create(
       url: url,
-      body: url.mechanize_page.body,
-      title: url.mechanize_page.title,
-      links: url.mechanize_page.links.map(&:resolved_uri )
+      body: mechanize_page.body,
+      title: mechanize_page.title,
+      links: mechanize_page.links.map(&:resolved_uri)
     )
   end
 end

35d87878c5b08a8ac1f7124c9f302209a2806323
Samuel Ballan <sgb4622@gmail.com>
2019-11-01 22:05:52 -0400

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_02_010740) do
+ActiveRecord::Schema.define(version: 2019_11_02_012846) do
 
   create_table "doc_bases", force: :cascade do |t|
     t.string "value"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "type"
+  end
+
+  create_table "urls", force: :cascade do |t|
+    t.string "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end

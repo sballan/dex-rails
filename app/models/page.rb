7e1b8fbb5cf318829b60be9579c8374e148042e7
Samuel Ballan <sgb4622@gmail.com>
2019-11-09 14:54:52 -0500

    page caching

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,40 +7,29 @@ class Page < ApplicationRecord
 
   validates :url_string, presence: true, uniqueness: true
 
-  value :cached_body, marshal: true, compress: true, expireat: { Time.now + 20.minutes }
-  value :cached_title, expireat: { Time.now + 20.minutes }
-  set   :cached_links, expireat: { Time.now + 20.minutes }
-
-  def uri
-    @uri ||= begin
-      URI(self[:url_string])
-    end
-  end
+  value :cached_body, marshal: true, compress: true, expireat: -> { Time.now + 20.minutes }
+  value :cached_title, expireat: -> { Time.now + 20.minutes }
+  set   :cached_links, expireat: -> { Time.now + 20.minutes }
 
   def cache_page(force = false)
-    unless cached_body.present? || force
+    if self.host.rate_limit_reached?
+      Rails.logger.debug "Rate limit reached, skipping #{self[:url_string]}"
+      return false
+    end
+
+    if cached_body.nil? || force
       cached_body.value = mechanize_page.body
-    end
 
-    unless cached_title.present? || force
       cached_title.value = mechanize_page.title
-    end
 
-    unless cached_links.present? || force
-      cached_links.clear
-      cached_links.merge mechanize_page.links.map do |mechanize_link|
-        mechanize_link.resolved_uri rescue nil
+      links = mechanize_page.links.map do |mechanize_link|
+        mechanize_link.resolved_uri.to_s rescue nil
       end.compact
-    end
-  end
 
-  def noko_doc
-    return nil unless cached_body.present?
-    @noko_doc ||= begin
-      require 'nokogiri'
-      doc = Nokogiri::HTML.parse(cached_page_body.value)
-      doc.xpath("//script").remove
-      doc
+      cached_links.clear
+      cached_links.merge links
+
+      return true
     end
   end
 
@@ -48,9 +37,24 @@ class Page < ApplicationRecord
     @mechanize_page || fetch_mechanize_page
   end
 
+  # @return [Nokogiri::HTML::Document]
+  def noko_doc
+    return nil unless cached_body.present?
+    @noko_doc ||= begin
+      require 'nokogiri'
+      doc = Nokogiri::HTML.parse(cached_body.value)
+      doc.xpath("//script").remove
+      doc
+    end
+  end
+
+  # @return [Mechanize::Page]
   def fetch_mechanize_page
     require 'mechanize'
     agent = Mechanize.new
+
+    self.host.increment_crawls
+
     @mechanize_page = agent.get(self[:url_string])
     return nil unless @mechanize_page.is_a?(Mechanize::Page)
 

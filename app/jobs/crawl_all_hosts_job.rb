e31589ed63596dba73b5eddfd3b7c91ed8141b6c
Samuel Ballan <sgb4622@gmail.com>
2019-11-11 03:08:48 -0500

    typo

--- app/jobs/crawl_all_hosts_job.rb
+++ app/jobs/crawl_all_hosts_job.rb
@@ -1,7 +1,7 @@
 class CrawlAllHostsJob < ApplicationJob
   queue_as :retry_crawling
 
-  def perform(urls)
+  def perform
     GC.start(full_mark: true, immediate_sweep: true)
     Host.all.to_a.shuffle[0..20].each do |host|
       host.crawl if host.found?

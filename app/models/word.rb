daa5074ddb20f84a8ff8e0b3d19e8b2b314f4400
Samuel Ballan <sgb4622@gmail.com>
2019-11-12 22:41:38 -0500

    word uses cache

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,10 +1,22 @@
 class Word < ApplicationRecord
   include Redis::Objects
 
-  has_many :page_words, dependent: :destroy
-  has_many :pages, through: :page_words
+  has_many :page_words, touch: true, dependent: :destroy
+  has_many :pages, touch: true, through: :page_words
 
   validates :value, presence: true, uniqueness: true
+
+  def cache_db_pages
+    Rails.cache.fetch("#{cache_key_with_version}/db_pages") do
+      self.pages
+    end
+  end
+
+  def cache_page_db_words
+    Rails.cache.fetch("#{cache_key_with_version}/page_db_words") do
+      self.page_words
+    end
+  end
 
   # serialize :freq_map, JSON
 

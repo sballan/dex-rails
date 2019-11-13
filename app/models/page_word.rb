a538471ade1fcfd6c28f7665ef803e9e49946297
Samuel Ballan <sgb4622@gmail.com>
2019-11-12 22:40:56 -0500

    pageword touch

--- app/models/page_word.rb
+++ app/models/page_word.rb
@@ -1,8 +1,8 @@
 class PageWord < ApplicationRecord
   include Redis::Objects
 
-  belongs_to :page
-  belongs_to :word
+  belongs_to :page, touch: true
+  belongs_to :word, touch: true # word caches page_words, so we need to touch it to invalidate its cache
 
   validates :page_count, presence: true
 end

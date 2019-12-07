3e5560f23ffa6054cc1932224cd0c1866dcbbc05
Samuel Ballan <sgb4622@gmail.com>
2019-12-07 11:13:38 -0500

    read from CACHE_REDIS_URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,8 +56,9 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  config.cache_store = :redis_cache_store
-
+  config.cache_store = :redis_cache_store, {
+    url: ENV['CACHE_REDIS_URL'] || ENV['REDIS_URL']
+  }
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "dex_rails_production"

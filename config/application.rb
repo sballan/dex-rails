73b50851f6390da0265113bd6f0d0c16bdcfcedd
Samuel Ballan <sgb4622@gmail.com>
2019-12-07 17:20:55 -0500

    silly redis check

--- config/application.rb
+++ config/application.rb
@@ -16,15 +16,6 @@ module DexRails
     config.eager_load_paths << Rails.root.join('lib')
     config.autoload_paths << Rails.root.join('lib')
 
-    redis_connected = begin
-                        !!Sidekiq.redis(&:info)
-                      rescue StandardError
-                        false
-                      end
-    unless redis_connected
-      raise 'No redis connection, run `bundle exec sidekiq`'
-    end
-
     # config.active_job.queue_adapter = :sidekiq
     config.active_job.queue_adapter = ENV.fetch('QUEUE_ADAPTER', 'sidekiq')
 

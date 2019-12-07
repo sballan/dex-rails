35c0bc146909e080dc99361328d95e8e68989355
Samuel Ballan <sgb4622@gmail.com>
2019-12-07 16:17:36 -0500

    let page do some things

--- app/models/page.rb
+++ app/models/page.rb
@@ -21,4 +21,22 @@ class Page < ApplicationRecord
       self.host ||= Host.find_or_create_by host_url_string: host_url_string
     end
   end
+
+  def recently_indexed?(index_interval = nil)
+    last_download = [
+      download_success.to_i || 0,
+      download_failure.to_i || 0,
+      download_invalid.to_i || 0
+    ].max
+
+    index__interval ||= page.host.success_retry_seconds
+    return false if Time.now < last_download + index__interval
+
+    true
+  end
+
+  def index_allowed?
+    return false unless host.allowed?(url_string)
+    return false unless host.rate_limit_reached?
+  end
 end

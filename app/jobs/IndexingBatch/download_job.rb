2ac9adc9283b6f3035063a2d04c73a3eda9c76a6
Samuel Ballan <sgb4622@gmail.com>
2019-12-08 13:54:09 -0500

    download job should discard and retry properly

--- app/jobs/IndexingBatch/download_job.rb
+++ app/jobs/IndexingBatch/download_job.rb
@@ -2,6 +2,12 @@ class IndexingBatch
   class DownloadJob < ApplicationJob
     queue_as :downloading
 
+    discard_on Page::BadCrawl
+
+    rescue_from(Page::LimitReached) do
+      retry_job queue: :downloading, wait: Random.rand((1.second)..(1.minute))
+    end
+
     # @param [::IndexingBatch] batch
     def perform(batch, page)
       batch.download_page(page)

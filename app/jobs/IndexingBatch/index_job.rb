64b776e8a7f14a2258cc0b30af75e0d070b2a93c
Samuel Ballan <sgb4622@gmail.com>
2019-12-15 23:13:02 -0500

    collect garbage after index

--- app/jobs/IndexingBatch/index_job.rb
+++ app/jobs/IndexingBatch/index_job.rb
@@ -7,6 +7,7 @@ class IndexingBatch
     # @param [::IndexingBatch] batch
     def perform(batch, page)
       batch.index_page(page)
+      GC.start(full_mark: true, immediate_sweep: true)
     end
   end
 end

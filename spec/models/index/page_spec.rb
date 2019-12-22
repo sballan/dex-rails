a10e803017c5a1e1cac1fee2225d511570eb11f9
Samuel Ballan <sgb4622@gmail.com>
2019-12-22 17:10:11 -0500

    test links

--- spec/models/index/page_spec.rb
+++ spec/models/index/page_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Index::Page, type: :model do
+  let(:host) { Index::Host.create(url_string: 'test.com') }
   it 'has a host' do
-    host = Index::Host.create(url_string: 'test.com')
     page = Index::Page.create(index_host: host, url_string: 'test.com')
     expect(page).to be
+  end
+
+  it 'has links array' do
+    page = Index::Page.create(index_host: host, url_string: 'test.com')
+    page.links = ['test1', 'test2']
+    page.save
+    expect(page.links.first).to eql('test1')
   end
 end

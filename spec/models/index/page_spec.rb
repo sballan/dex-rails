e28d96775ccdf0c134c013caa125181e0001d007
Samuel Ballan <sgb4622@gmail.com>
2019-12-22 17:44:22 -0500

    host not needed

--- spec/models/index/page_spec.rb
+++ spec/models/index/page_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Index::Page, type: :model do
-  let(:host) { Index::Host.create(url_string: 'test.com') }
-  it 'has a host' do
+  it 'can be created with a host' do
+    host = Index::Host.create(url_string: 'test.com')
     page = Index::Page.create(host: host, url_string: 'test.com')
     expect(page).to be
   end
 
+  it 'can be created without a host' do
+    page = Index::Page.create(url_string: 'test.com')
+    expect(page).to be
+  end
+
   it 'has links array' do
-    page = Index::Page.create(host: host, url_string: 'test.com')
+    page = Index::Page.create(url_string: 'test.com')
     page.links = ['test1', 'test2']
     page.save
     expect(page.links.first).to eql('test1')

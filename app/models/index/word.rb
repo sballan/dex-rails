971667b02e04a577120f6422dd590c7b87b2db0e
Samuel Ballan <sgb4622@gmail.com>
2019-12-22 21:47:41 -0500

    word pagewords

--- app/models/index/word.rb
+++ app/models/index/word.rb
@@ -1,3 +1,5 @@
 class Index::Word < ApplicationRecord
+  has_many :page_words, class_name: 'Index::PageWord', foreign_key: :index_word_id
+
   validates :value, presence: true
 end
--- spec/models/index/word_spec.rb
+++ spec/models/index/word_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Index::Word, type: :model do
   it 'can be created with a value' do
-    word = Index::Word.create(value: 'test')
+    word = Index::Word.create!(value: 'test')
     expect(word).to be
+  end
+
+  it 'can have page_words' do
+    page = Index::Page.create(url_string: 'test.com')
+    word = Index::Word.create!(value: 'test')
+    word.page_words.create page: page
   end
 end

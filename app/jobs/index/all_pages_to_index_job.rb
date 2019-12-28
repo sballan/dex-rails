b9738ef9eac97f625666d9b8acef7703ddbcd7cf
Samuel Ballan <sgb4622@gmail.com>
2019-12-28 00:53:30 -0500

    fix job name

--- app/jobs/index/all_pages_to_index_job.rb
+++ app/jobs/index/all_pages_to_index_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Index
-  class IndexAllPagesJob < ApplicationJob
+  class AllPagesToIndexJob < ApplicationJob
     queue_as :indexing
 
     def perform(limit = nil)

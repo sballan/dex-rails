bc37a74cac4663dc4a66545b4a6be7dc0a0c88e3
Samuel Ballan <sgb4622@gmail.com>
2019-12-28 11:53:59 -0500

    safe queueing across hosts

--- app/jobs/index/queue_fetch_pages_job.rb
+++ app/jobs/index/queue_fetch_pages_job.rb
@@ -5,7 +5,12 @@ module Index
     queue_as :indexing
 
     def perform(limit = 10)
+      queued_hosts = Set.new
+
       Index.all_pages_to_fetch(limit).in_batches.each_record do |record|
+        next if queued_hosts.include?(record.index_host_id)
+
+        queued_hosts << record.index_host_id
         FetchPageJob.perform_later(record)
       end
     end

77a6c1ec88b69f7a705f1bae1f80427f0ae9f0ce
Samuel Ballan <sgb4622@gmail.com>
2019-12-28 15:24:29 -0500

    merge

---
+++ app/jobs/index/queue_index_pages_job.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Index
+  class QueueIndexPagesJob < ApplicationJob
+    queue_as :indexing
+
+    def perform(limit = 10)
+      Index::Page.all.in_batches.each_record do |record|
+        Index::IndexPageJob.perform_later(record)
+      end
+    end
+  end
+end

233103946250428e66a05b01aa9c21ac8a0259aa
Samuel Ballan <sgb4622@gmail.com>
2020-01-05 02:24:15 -0500

    bigger slices for index

--- app/models/index/page.rb
+++ app/models/index/page.rb
@@ -47,7 +47,7 @@ class Index::Page < ApplicationRecord
   def index_page
     raise "can't index before download" unless downloads.any?
 
-    generate_page_word_data.each_slice(100) do |slice|
+    generate_page_word_data.each_slice(150) do |slice|
       Index::PageWord.upsert_all(
         slice,
         unique_by: :index_index_page_words_on_index_word_id_and_index_page_id
